352b550a699461aba640b45a53771017e64f5be4
calvinmcelvain <mcelvain1@hotmail.com>
2024-12-30 14:04:03 -0600

    :test_tube: test: Adding test args

--- tests/base_tests.py
+++ tests/base_tests.py
@@ -12,3 +12,11 @@ importlib.reload(irpd)
 
 # Base testing instance
 base_tests = irpd.IRPD(dir_path="/Users/fogellmcmuffin/Dropbox/ai_irpd_coding/")
+
+# Testing args
+instance = 'uni'
+ras = 'eli'
+stages = ['1', '1r', '1c', '2', '3']
+treatments = 'noise'
+sub_test_args = {'instance': instance, 'ras': ras, 'stages': stages, 'treatments': treatments, 'test_type': 'subtest'}
+test_test_args = {'instance': instance, 'ras': ras, 'stages': stages, 'treatments': treatments, 'test_type': 'test'}
